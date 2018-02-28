daee2e0d82485b7b531b50ae118fff4c72bc1914
jackchuka <jack16.m.u@gmail.com>
2018-02-28 10:53:04 +0900

    refactor: minor

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import MeCab
 import re
+import numpy as np
 import pandas as pd
 from statistics import mean
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -22,6 +23,7 @@ def morph(sentence, option='名詞'):
                 pass
         node = node.next
 
+
 def vectorize(document):
     np.set_printoptions()
     docs = np.array(document)
@@ -38,28 +40,30 @@ def vectorize(document):
             if freq > 0:
                 print(freq, word_vector[index])
 
+
 def get_diclist(text):
     tagger = MeCab.Tagger()
-    parsed = tagger.parse(text)      # 形態素解析結果（改行を含む文字列として得られる）
+    parsed = tagger.parse(text)  # 形態素解析結果（改行を含む文字列として得られる）
     lines = parsed.split('\n')  # 解析結果を1行（1語）ごとに分けてリストにする
-    lines = lines[0:-2]         # 後ろ2行は不要なので削除
+    lines = lines[0:-2]  # 後ろ2行は不要なので削除
     diclist = []
     for word in lines:
-        l = re.split('\t|,',word)  # 各行はタブとカンマで区切られてるので
-        d = {'Surface':l[0], 'POS1':l[1], 'POS2':l[2], 'BaseForm':l[7]}
+        l = re.split('\t|,', word)  # 各行はタブとカンマで区切られてるので
+        d = {'Surface': l[0], 'POS1': l[1], 'POS2': l[2], 'BaseForm': l[7]}
         diclist.append(d)
-    return(diclist)
+    return diclist
+
 
 def add_pnvalue(diclist_old):
-    pn_df = pd.read_csv('dictionary/pn_ja.dic.txt',\
-                    sep=':',
-                    encoding='Shift-JIS',
-                    names=('Word','Reading','POS', 'PN')
-                   )
+    pn_df = pd.read_csv(
+        'dictionary/pn_ja.dic.txt',
+        sep=':',
+        encoding='Shift-JIS',
+        names=('Word', 'Reading', 'POS', 'PN')
+    )
     word_list = list(pn_df['Word'])
     pn_list = list(pn_df['PN'])  # 中身の型はnumpy.float64
     pn_dict = dict(zip(word_list, pn_list))
-
 
     diclist_new = []
     for word in diclist_old:
@@ -70,7 +74,8 @@ def add_pnvalue(diclist_old):
             pn = 'notfound'
         word['PN'] = pn
         diclist_new.append(word)
-    return(diclist_new)
+    return diclist_new
+
 
 def get_pnmean(diclist):
     pn_list = []
@@ -78,8 +83,8 @@ def get_pnmean(diclist):
         pn = word['PN']
         if pn != 'notfound':
             pn_list.append(pn)  # notfoundだった場合は追加もしない
-    if len(pn_list) > 0:        # 「全部notfound」じゃなければ
+    if len(pn_list) > 0:  # 「全部notfound」じゃなければ
         pnmean = mean(pn_list)
     else:
-        pnmean = 0              # 全部notfoundならゼロにする
-    return(pnmean)
+        pnmean = 0  # 全部notfoundならゼロにする
+    return (pnmean)
