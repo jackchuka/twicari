3452e97e7326d6c27703fe16f3ff3cc331c2a97c
inatonix <imitation0813@gmail.com>
2018-02-28 10:43:45 +0900

    add funcs for pn

--- analyze.py
+++ analyze.py
@@ -1,4 +1,7 @@
 import MeCab
+import re
+import pandas as pd
+from statistics import mean
 
 
 def separate_texts(text):
@@ -13,3 +16,49 @@ def morph(text, option='名詞'):
         if node.feature.startswith(option):
             return node.surface
         node = node.next
+
+def get_diclist(text):
+    tagger = MeCab.Tagger()
+    parsed = tagger.parse(text)      # 形態素解析結果（改行を含む文字列として得られる）
+    lines = parsed.split('\n')  # 解析結果を1行（1語）ごとに分けてリストにする
+    lines = lines[0:-2]         # 後ろ2行は不要なので削除
+    diclist = []
+    for word in lines:
+        l = re.split('\t|,',word)  # 各行はタブとカンマで区切られてるので
+        d = {'Surface':l[0], 'POS1':l[1], 'POS2':l[2], 'BaseForm':l[7]}
+        diclist.append(d)
+    return(diclist)
+
+def add_pnvalue(diclist_old):
+    pn_df = pd.read_csv('dictionary/pn_ja.dic.txt',\
+                    sep=':',
+                    encoding='Shift-JIS',
+                    names=('Word','Reading','POS', 'PN')
+                   )
+    word_list = list(pn_df['Word'])
+    pn_list = list(pn_df['PN'])  # 中身の型はnumpy.float64
+    pn_dict = dict(zip(word_list, pn_list))
+
+
+    diclist_new = []
+    for word in diclist_old:
+        base = word['BaseForm']     
+        if base in pn_dict:
+            pn = float(pn_dict[base]) 
+        else:
+            pn = 'notfound'            
+        word['PN'] = pn
+        diclist_new.append(word)
+    return(diclist_new)
+
+def get_pnmean(diclist):
+    pn_list = []
+    for word in diclist:
+        pn = word['PN']
+        if pn != 'notfound':
+            pn_list.append(pn)  # notfoundだった場合は追加もしない
+    if len(pn_list) > 0:        # 「全部notfound」じゃなければ
+        pnmean = mean(pn_list)
+    else:
+        pnmean = 0              # 全部notfoundならゼロにする
+    return(pnmean)
