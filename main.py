8755bc003ba8dff91687534120f6ec903bf46691
inatonix <imitation0813@gmail.com>
2018-02-28 11:05:40 +0900

    enable to use PN:

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ api = TwitterAPI(config.CONSUMER_KEY, config.CONSUMER_SECRET, config.ACCESS_TOKE
 if __name__ == '__main__':
     max_id = None
     document = []
+    pnmean_list = []
+    text_list = []
+
     for i in range(1):
         r = api.request('search/tweets', {
             'q': 'メルカリ',
@@ -15,6 +18,8 @@ if __name__ == '__main__':
             'max_id': max_id
         })
         for item in r:
+            text_list.append(item['text'])
+            pnmean_list.append(analyze.get_pnmean(analyze.add_pnvalue(analyze.get_diclist(item['text']))))
             document.append(analyze.morph(item['text']))
             max_id = item['id_str']
     analyze.vectorize(document)
